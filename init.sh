ca5f28896a031dcea55c39c1cbc6758460e2d8ad
broadiq <33910956+broadiq@users.noreply.github.com>
2018-11-12 18:03:58 -0700

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,22 @@
 #! /bin/sh
 
-chmod -R 777 /exports
+while true
+do
+
+if [ -d "/exports" ]; then
+
+echo "directory exist...";
+
+
+ chmod -R 777 /exports;
+
+break;
+fi
+
+sleep $(($RANDOM % 5 + 5));
+
+done
+
+echo "done with update....";
+
+
