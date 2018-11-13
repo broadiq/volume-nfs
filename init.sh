5cd5f6942b262753af99ffa5200ef759d6610fe1
broadiq <33910956+broadiq@users.noreply.github.com>
2018-11-12 18:50:37 -0700

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,26 +1,6 @@
 #! /bin/sh
 
-/usr/local/bin/run_nfs.sh &
+ 
+/permissions.sh &
 
-
-
-while true
-do
-
-if [ -d "/exports" ]; then
-
-echo "directory exist...";
-
-
- chmod -R 777 /exports;
-
-break;
-fi
-
-sleep $(($RANDOM % 5 + 5));
-
-done
-
-echo "done with update....";
-
-
+/usr/local/bin/run_nfs.sh
