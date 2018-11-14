206dba46d6b05680584af9edea779e4cd1c10ce5
broadiq <jlutz@broadiq.com>
2018-11-14 09:55:00 -0700

    updated file

--- run_nfs_briq.sh
+++ run_nfs_briq.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 # Copyright 2015 The Kubernetes Authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,8 +13,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 function start()
 {
+
     unset gid
     # accept "-G gid" option
     while getopts "G:" opt; do
@@ -22,6 +25,7 @@ function start()
         esac
     done
     shift $(($OPTIND - 1))
+
     # prepare /etc/exports
     for i in "$@"; do
         # fsid=0: needed for NFSv4
@@ -33,37 +37,47 @@ function start()
         # move index.html to here
         /bin/cp /tmp/index.html $i/
         chmod 644 $i/index.html
-        #chmod 777 -R $i
         echo "Serving $i"
     done
+  
     # start rpcbind if it is not started yet
     /usr/sbin/rpcinfo 127.0.0.1 > /dev/null; s=$?
     if [ $s -ne 0 ]; then
        echo "Starting rpcbind"
        /usr/sbin/rpcbind -w
     fi
+
     mount -t nfsd nfds /proc/fs/nfsd
+
     # -V 3: enable NFSv3
     /usr/sbin/rpc.mountd -N 2 -V 3
+
     /usr/sbin/exportfs -r
     # -G 10 to reduce grace time to 10 seconds (the lowest allowed)
     /usr/sbin/rpc.nfsd -G 10 -N 2 -V 3
     /usr/sbin/rpc.statd --no-notify
     echo "NFS started"
 }
+
 function stop()
 {
     echo "Stopping NFS"
+
     /usr/sbin/rpc.nfsd 0
     /usr/sbin/exportfs -au
     /usr/sbin/exportfs -f
+
     kill $( pidof rpc.mountd )
     umount /proc/fs/nfsd
     echo > /etc/exports
     exit 0
 }
+
+
 trap stop TERM
+
 start "$@"
+
 # Ugly hack to do nothing and wait for SIGTERM
 while true; do
     sleep 5
